ebc610f5e2ebf6fa2402a0a87d63e49307ec1ab4
Siddharth Singh <singhsiddharth1102@gmail.com>
2023-07-08 00:38:49 +0530

    1 day complete

--- 1day.cpp
+++ 1day.cpp
@@ -15,3 +15,35 @@ public:
     }
 };
 
+// ------ Parking System -----//
+
+class ParkingSystem {
+public:
+int  B = 0;
+int M = 0;
+ int S = 0;
+    ParkingSystem(int big, int medium, int small) {
+        B = big;
+        M = medium;
+        S = small;
+       
+
+    }
+    
+    bool addCar(int carType) {
+         if (carType== 1){
+            B--;
+            if(B>=0)return 1;
+        }
+          if (carType== 2){
+            M--;
+            if(M>=0)return 1;
+        }
+          if (carType== 3){
+            S--;
+            if(S>=0)return 1;
+        }
+        return 0;
+    }
+};
+
